7d182cb311c97a697d7e49d016d06bcc18c2ac92
NicolasMC94 <mcnicolas.94@gmail.com>
2022-07-29 19:25:20 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generate(who, what, when) {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return randomWho + " " + randomWhat + " " + randomWhen;
+  }
+
+  console.log(generate(who, what, when));
+  document.getElementById("excuse").innerHTML = generate(who, what, when);
 };
